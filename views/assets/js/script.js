c1c9338cc2fc3ba8b40ab7d1b02badfd07fb4bba
PrLamkin <shiektheimmortal@outlook.com>
2020-11-04 17:37:59 -0600

    back up

---
+++ views/assets/js/script.js
@@ -0,0 +1,71 @@
+let cardElement = document.querySelector(".card");
+
+cardElement.addEventListener("click", flip);
+
+const { query } = require("express");
+
+function flip() {
+    cardElement.classList.toggle("flipped")
+}
+
+function startTime() {
+    var weekday = new Array();
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var d = today.getDate();
+    var y = today.getFullYear();
+    var wd = weekday[today.getDay()];
+    var mt = month[today.getMonth()];
+
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date').innerHTML =
+        d;
+    document.getElementById('day').innerHTML =
+        wd;
+    document.getElementById('month').innerHTML =
+        mt + "/" + y;
+
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
+}
+
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-discription p");
+const locationElement = document.querySelector(".location p");
+
+const weather = {};
+
+weather.temperature = {
+    unit = "fehrenheight"
+}
+
+const KELVIN = 273;
+const key = "c469fa08a7e34c2dbca8903af9a019bb"
